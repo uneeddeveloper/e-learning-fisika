a196224a30889564e556cef08f3a4f0a19249858
Kyoze <hrmnfrmnsyh1210@gmail.com>
2026-04-07 19:04:13 +0700

    setup database ke supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: ['@nuxtjs/supabase'],
+
+  supabase: {
+    redirectOptions: {
+      login: '/auth/login',
+      callback: '/auth/confirm',
+      exclude: ['/', '/courses', '/courses/*'],
+    },
+  },
+
+  runtimeConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+  },
 })
